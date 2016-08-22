73f2e7d8cc941aa2c96da8f8ac982dd5dbe9e1d5
Skimer <savok92@gmail.com>
2016-08-22 13:36:02 +0300

    Update directory_checker.py

--- directory_checker.py
+++ directory_checker.py
@@ -3,12 +3,12 @@ import subprocess
 subprocess.call(["rsync", "10.0.2.54", "-rvazh", "/usr/src","/usr/src/",])
 
 # We need to connect to remote host, than check for existing file / directory
-#def check_dir_exist(os_dir):
-    #if not os.path.exists(os_dir):
-       # print os_dir, "does not exist."
-    #else:
+def check_dir_exist(os_dir):
+    if not os.path.exists(os_dir):
+        print os_dir, "does not exist."
+    else:
         # This command copies data recursively from local host to remote host
-     #   os.system("rsync -avrz /opt/data/filename root@ip:/opt/data/file")
+    subprocess.call(["rsync", "10.0.2.54", "-rvazh", "/usr/src","/usr/src/",])
                 # -r copies data recursively
                 #- v verbose
                 #- a : archive mode, archive mode allows copying files recursively and it also
