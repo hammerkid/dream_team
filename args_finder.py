6980dbe8f303e0c979ebff25448a8f9f4196d470
hammerkid <hammerkid123@gmail.com>
2016-08-22 17:16:57 +0300

    test version for convert args

--- args_finder.py
+++ args_finder.py
@@ -1,16 +1,58 @@
+import re
 import sys
 
-path_list = []
+def passwd_arg():
+    search_passwd = re.compile(r'\b[p][a][s].*')
+    for args in sys.argv:
+        #print args
+        try:
+            passwd = ('-' + search_passwd.search(args).group())
+            return passwd
+        except:
+            pass
 
-for argument in sys.argv:
-    if '@' and ':' or ',' or '.'  in argument:
-        IP_adress = argument
-        print IP_adress
-    if argument.startswith('-'):
-        rsync_opt = argument
-    if '/' in argument:
-        path_list.append(argument)
-    if '.txt' in argument:
-        txt_file = argument
-    if '.avi' in argument:
-        avi_file = argument
+#a = passwd_arg()
+#print a
+
+
+
+def opts_arg():
+    allow_opts = ['P', 'a', 'v', 'S', 'z', 'q', 'progress', 'e']
+    opts = []
+    search_opts = re.compile(r'\-+.*')
+    for args in sys.argv:
+        try:
+            x=[]
+            opts = (search_opts.search(args).group())
+            for opt in opts:
+                if opt in allow_opts:
+                    x.append(opt)
+                    print x
+        except:
+            pass
+
+
+optsx = opts_arg()
+print optsx
+
+def path_arg():
+    path = []
+    search_path = re.compile('\/+.*')
+    for args in sys.argv:
+        try:
+            path.append(search_path.search(args).group())
+            return path
+        except:
+            pass
+
+
+def IP_arg():
+    search_IP = re.compile(r'\w*,\w*@\w*:\w*')
+    for args in sys.argv:
+        try:
+            IP = search_IP.search(args).group()
+            return IP
+        except:
+            pass
+
+
