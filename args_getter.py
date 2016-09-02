1f606417b7f830e2b7897c95a1461a226c9f27b9
hammerkid <hammerkid123@yandex.ru>
2016-09-02 17:28:29 +0300

    *

--- args_getter.py
+++ args_getter.py
@@ -1,28 +1,36 @@
-import sys
-import re
+import sys, re, os, paramiko
 
-search_IP = re.compile(r'\w*,\w*@\w*:\w*')       #pattern for search remote name or IP
-search_path = re.compile('\/+.*')                #pattern for search path 1st-from-path 2nd-dest-path
-search_opts = re.compile(r'\-+.*')               #pattern for search opts for rsync(need more check)
-search_passwd = re.compile('[p][a][s].*')        #pattern for optional -pass
+
+search_address = re.compile(r'\w*,\w*@\w*:\w*')   # pattern for search remote name or IP
+search_path = re.compile('\/+.*')                # pattern for search path 1st-from-path 2nd-dest-path
+search_opts = re.compile(r'\-+.*')               # pattern for search opts for rsync(need more check)
+search_passwd = re.compile('[p][a][s].*')        # pattern for optional -pass
+search_IP = re.compile(r'^[^@]*')                # get ip from address
 path = []
 opts = []
-#passwd =''
+
 for args in sys.argv:
-
-    #print args
     try:
-         passwd = search_passwd.findall(args)
-         print passwd
-         path.append(search_path.search(args).group())
-         print path
-         IP = search_IP.search(args).group()
-         print IP
-         opts.append(search_opts.search(args).group())
-         print opts
-
-
+        passwd = search_passwd.findall(args)
+        path.append(search_path.search(args).group())
+        address = search_address.search(args).group()
+        opts.append(search_opts.search(args).group())
+        ip = (search_IP.search(address).group())
     except:
         print ('---')
 
+response = os.system("ping -c 1 " + ip)
+if response == 0:                 # check the response...
+    print ip, 'is up!'
+else:
+    print ip, 'is down!'
 
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())   # need check installed rsync on remote
+client.connect(hostname=ip, username=address, password=passwd) # sudo apt-get install rsync or sudo yum -y install rsync
+stdin, stdout, stderr = client.exec_command('sudo apt-get install rsync | sudo yum -y install rsync')
+stdin.write(passwd)
+stdin.flush()
+data = stdout.read() + stderr.read()
+client.close()
